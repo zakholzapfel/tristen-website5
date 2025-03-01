1c39df12916f01c15dca18b99a443594d0a962ec
zakholzapfel <zak.holzapfel@gmail.com>
2025-03-01 17:42:38 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let cartItems = [];
-    
-    document.getElementById("contact-form").addEventListener("submit", function(event) {
-        event.preventDefault();
-        alert("Message sent successfully!");
-    });
-});
+document.addEventListener("DOMContentLoaded", function() { let cartItems = []; });
