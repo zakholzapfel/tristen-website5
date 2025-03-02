d3671d4e3ebbda51d6d87dafa651f500ffa13ad3
zakholzapfel <zak.holzapfel@gmail.com>
2025-03-01 19:36:28 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let cartItems = [];
-    document.querySelectorAll(".home-button").forEach(button => {
-        button.addEventListener("click", () => {
-            window.location.href = "index.html";
-        });
-    });
-});
+<PASTE THE FULL script.js CODE HERE>
